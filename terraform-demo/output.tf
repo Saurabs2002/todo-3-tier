95a85ac8e7693063c070fc25007fec1de8642d95
Saurabs2002 <129584008+Saurabs2002@users.noreply.github.com>
2026-08-29 17:05:33 +0000

    Update output.tf

--- terraform-demo/output.tf
+++ terraform-demo/output.tf
@@ -1,3 +1,4 @@
-output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
+output "public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.my_ec2.public_ip
 }
